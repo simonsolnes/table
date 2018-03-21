815e90213bca1ffa7355b6764d52f528ba9c473c
Simon Solnes <simon@solnes.co>
2018-03-21 15:01:27 +0100

    implemented a join funciton for colors

--- __init__.py
+++ __init__.py
@@ -8,22 +8,30 @@ class Table():
             while len(line) < self.cols:
                 line.append('')
         self.lens = [max([len(item) for item in row]) for row in zip(*self.data)]
+    def join(self, ch, parts):
+        ret = ''
+        for idx, part in enumerate(parts):
+            ret += str(part)
+            if idx < len(parts) - 1:
+                ret += str(ch)
+            
+        return ret
 
     def __str__(self):
-        top = '┌' + ''.join([('─' * l) + '┬'  for l in self.lens])[:-1] + '┐'
+        top = '┌' + self.join('', [('─' * l) + '┬'  for l in self.lens])[:-1] + '┐'
         retval = [top]
         for line in self.data:
             retline = []
             for item, just in zip(line, self.lens):
                 retline.append(item.ljust(just))
-            retval.append('│' + '│'.join(retline) + '│')
-        bot = '└' + ''.join([('─' * l) + '┴'  for l in self.lens])[:-1] + '┘'
+            retval.append('│' + self.join('│', retline) + '│')
+        bot = '└' + self.join('', [('─' * l) + '┴'  for l in self.lens])[:-1] + '┘'
         retval.append(bot)
-        hed = '├' + ''.join([('─' * l) + '┼'  for l in self.lens])[:-1] + '┤'
+        hed = '├' + self.join('', [('─' * l) + '┼'  for l in self.lens])[:-1] + '┤'
         retval.insert(2, hed)
                 
 
-        return '\n'.join(retval)
+        return self.join('\n', retval)
 
 
 
